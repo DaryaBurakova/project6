9023b490e3ee8826646ad5485087433ff3fdbc37
roman <evgenipetrenko170674@gmail.com>
2024-06-26 11:01:37 +0600

    function

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from src.masks import get_mask_card_number, get_mask_account
+from datetime import datetime
+
+
+def mask_account_card(string: str) -> str:
+    """Маскирует номер карты или счета"""
+    if "Счет" in string:
+        return f"Счет {get_mask_account(string)}"
+    else:
+        card = get_mask_card_number(string[-16:])
+        mask_card = string.replace(string[-16:], card)
+        return mask_card
+
+
+def get_data(data: str) -> str:
+    """Функция, которая возвращает дату"""
+    d = datetime.strptime(data, format("%Y-%m-%dt%H:%M:%S.%f"))
+    return d.strftime("%d.%m.%Y")
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa platinum 1111222233334444"))
+    print(mask_account_card("Счет 1111222233335555"))
+    print(get_data("2018-07-11T02:26:18.671407"))
