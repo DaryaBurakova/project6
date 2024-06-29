7d61b32297c00eaa6a0cc740d77073b12bf8884d
roman <evgenipetrenko170674@gmail.com>
2024-06-29 22:53:49 +0600

    sorts by date decsending

--- src/processing.py
+++ src/processing.py
@@ -10,6 +10,12 @@ def filter_by_state(original_list: list[dict[str, Any]], state_id: str = "EXECUT
     return sorted_list
 
 
+def sort_by_date(original_list: list[dict[str, Any]], reverse_list: bool = True) -> Any:
+    """Сортирует список словарей по убыванию даты"""
+    sorted_list = sorted(original_list, key=lambda d: d["date"], reverse=reverse_list)
+    return sorted_list
+
+
 if __name__ == "__main__":
     print(
         filter_by_state(
@@ -21,3 +27,14 @@ if __name__ == "__main__":
             ]
         )
     )
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
